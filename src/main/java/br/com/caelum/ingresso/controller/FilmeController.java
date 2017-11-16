10f0320de79c52d0e6a3671b16d25d77407477ed
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-11-16 18:44:05 -0200

    consumindo serviço para detalhes do filme

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.ImdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -27,62 +29,60 @@ import br.com.caelum.ingresso.model.Sessao;
 @Controller
 public class FilmeController {
 
-
-    @Autowired
-    private FilmeDao filmeDao;
+	@Autowired
+	private FilmeDao filmeDao;
 
 	@Autowired
 	private SessaoDao sessaoDao;
 
+	@Autowired
+	private ImdbClient client;
 
-    @GetMapping({"/admin/filme", "/admin/filme/{id}"})
-    public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
+	@GetMapping({ "/admin/filme", "/admin/filme/{id}" })
+	public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme) {
 
-        ModelAndView modelAndView = new ModelAndView("filme/filme");
+		ModelAndView modelAndView = new ModelAndView("filme/filme");
 
-        if (id.isPresent()){
-            filme = filmeDao.findOne(id.get());
-        }
+		if (id.isPresent()) {
+			filme = filmeDao.findOne(id.get());
+		}
 
-        modelAndView.addObject("filme", filme);
+		modelAndView.addObject("filme", filme);
 
-        return modelAndView;
-    }
+		return modelAndView;
+	}
 
+	@PostMapping("/admin/filme")
+	@Transactional
+	public ModelAndView salva(@Valid Filme filme, BindingResult result) {
 
-    @PostMapping("/admin/filme")
-    @Transactional
-    public ModelAndView salva(@Valid Filme filme, BindingResult result){
+		if (result.hasErrors()) {
+			return form(Optional.ofNullable(filme.getId()), filme);
+		}
 
-        if (result.hasErrors()) {
-            return form(Optional.ofNullable(filme.getId()), filme);
-        }
+		filmeDao.save(filme);
 
-        filmeDao.save(filme);
+		ModelAndView view = new ModelAndView("redirect:/admin/filmes");
 
-        ModelAndView view = new ModelAndView("redirect:/admin/filmes");
+		return view;
+	}
 
-        return view;
-    }
+	@GetMapping(value = "/admin/filmes")
+	public ModelAndView lista() {
 
+		ModelAndView modelAndView = new ModelAndView("filme/lista");
 
-    @GetMapping(value="/admin/filmes")
-    public ModelAndView lista(){
+		modelAndView.addObject("filmes", filmeDao.findAll());
 
-        ModelAndView modelAndView = new ModelAndView("filme/lista");
+		return modelAndView;
+	}
 
-        modelAndView.addObject("filmes", filmeDao.findAll());
-
-        return modelAndView;
-    }
-
-
-    @DeleteMapping("/admin/filme/{id}")
-    @ResponseBody
-    @Transactional
-    public void delete(@PathVariable("id") Integer id){
-        filmeDao.delete(id);
-    }
+	@DeleteMapping("/admin/filme/{id}")
+	@ResponseBody
+	@Transactional
+	public void delete(@PathVariable("id") Integer id) {
+		filmeDao.delete(id);
+	}
 
 	@GetMapping("/filme/em-cartaz")
 	public ModelAndView emCartaz() {
@@ -96,7 +96,12 @@ public class FilmeController {
 		ModelAndView modelAndView = new ModelAndView("/filme/detalhe");
 		Filme filme = filmeDao.findOne(id);
 		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+		
+		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+		
 		modelAndView.addObject("sessoes", sessoes);
+		modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
+		
 		return modelAndView;
 	}
 
