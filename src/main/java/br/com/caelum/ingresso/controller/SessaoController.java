da535c8a9642271b516fe21e8911b7b8aa1388ff
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-11-21 08:33:28 -0200

    [MELHORIA] mensagem quando já existe sessão no horário

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,8 @@ public class SessaoController {
 			ModelAndView modelAndView =  new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 			sessaoDao.save(sessao);
 			return modelAndView;
+		} else {
+			result.addError(new FieldError("sessaoForm", "horario", "Já existe uma sessão nesse horário."));
 		}
 		return form(form.getSalaId(), form);
 
